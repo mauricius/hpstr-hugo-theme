f65a91ccf06e23442b677eeb38bda6bf30e1c140
mauricius <maurizio.bonani@gmail.com>
2020-01-29 17:39:31 +0100

    Remove jquery dependency

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -1,49 +1,14 @@
-/*! Plugin options and other jQuery stuff */
+var entryImg = document.querySelector('.entry-image img');
 
-// dl-menu options
-$(function() {
-  $( '#dl-menu' ).dlmenu({
-    animationClasses : { classin : 'dl-animate-in', classout : 'dl-animate-out' }
+if (entryImg) {
+  window.addEventListener('scroll', function() {
+    var filterVal = window.pageYOffset / 40;
+    var filter = 'blur(' + filterVal + 'px)';
+
+    entryImg.style.filter = filter;
+    entryImg.style.webkitFilter = filter;
+    entryImg.style.mozFilter = filter;
+    entryImg.style.oFilter = filter;
+    entryImg.style.msFilter = filter;
   });
-});
-
-// FitVids options
-$(function() {
-  $("article").fitVids();
-});
-
-$(".close-menu").click(function () {
-  $(".menu").toggleClass("disabled");
-  $(".links").toggleClass("enabled");
-});
-
-$(".about").click(function () {
-  $("#about").css('display','block');
-});
-
-$(".close-about").click(function () {
-  $("#about").css('display','');
-});
-
-// Add lightbox class to all image links
-$("a[href$='.jpg'],a[href$='.jpeg'],a[href$='.JPG'],a[href$='.png'],a[href$='.gif']").addClass("image-popup");
-
-// Magnific-Popup options
-$(document).ready(function() {
-  $('.image-popup').magnificPopup({
-    type: 'image',
-    tLoading: 'Loading image #%curr%...',
-    gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-    },
-    image: {
-      tError: '<a href="%url%">Image #%curr%</a> could not be loaded.',
-    },
-    removalDelay: 300, // Delay in milliseconds before popup is removed
-    // Class that is added to body when popup is open. 
-    // make it unique to apply your CSS animations just to this exact popup
-    mainClass: 'mfp-fade'
-  });
-});
+}
